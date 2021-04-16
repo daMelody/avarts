4b21686142911b809c6bf5c5bb77b3910c80b8d3
Daniel Melody <dmwalter12@gmail.com>
2021-04-15 21:08:38 -0400

    settled on the initial skeleton of the CLI using structopt I should really start using branches instead of committing straight to `main`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
-fn main() -> Result<()> {
-    let mut res = reqwest::blocking::get("https://");
-    Ok(())
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "enable your data-paranoid, athletic self")]
+struct Cli {
+    /// the client id belonging to the `avarts` app
+    #[structopt(short, long)]
+    client: String,
+    /// the secret id belonging to the `avarts` app
+    #[structopt(short, long)]
+    secret: String,
+    /// carry out the desired functionality
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Manually get some amount of activities
+    Grab {},
+    Watch {},
+}
+
+fn main() {
+    let opt = Cli::from_args();
+    println!("{:?}", opt);
 }
